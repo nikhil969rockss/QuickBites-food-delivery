73c59641a662383af5f843b3e194f5495b26e18b
Nikhil Randhawa <ns9013604867@gmail.com>
2026-03-20 17:13:42 +0530

    add error middleware for centralized error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,20 @@
+//library
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
 dotenv.config()
+
+
+//middlewares imports
+import errorMiddleware from './middlewares/error.middlerware.js'
 
 
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(errorMiddleware)
 
 export default app;
---
+++ Backend/src/middlewares/error.middlerware.js
@@ -0,0 +1,32 @@
+import ApiError from '../utils/ApiError.js'
+
+
+/**
+ * @description middleware to catch all the errors related to third party services and API's
+ * @param {import('express').ErrorRequestHandler} err - Error handler
+ * @param {import('express').Request} req - request object
+ * @param {import('express').Response} res - response object
+ * @param {import('express').NextFunction} next - next function
+ * 
+ */
+function errorMiddleware(err, req, res, next) {
+    let error = err
+
+    if (!(error instanceof ApiError)) {
+        console.log(error)
+
+    }
+    const response = {
+        success: error.success,
+        stausCode: error.statusCode,
+        message: error.message,
+        errors: error?.errors || [],
+        stack: (process.env.NODE_ENV === 'development' ? error.stack : null)
+
+    }
+
+    return res.status(error.statusCode || 500).json(response)
+
+}
+
+export default errorMiddleware
