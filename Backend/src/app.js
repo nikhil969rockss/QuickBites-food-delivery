00842042d16bbf361b3e56ebfb10b30632c80e6c
Nikhil Randhawa <ns9013604867@gmail.com>
2026-03-21 10:59:19 +0530

    feat: add CORS support to the application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 dotenv.config();
 
@@ -16,6 +17,10 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials:true
+}))
 
 //routes
 
