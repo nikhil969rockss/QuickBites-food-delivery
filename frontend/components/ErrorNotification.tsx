ebc1470278f889c947d3717feaf2bd66ba206159
Nikhil Randhawa <ns9013604867@gmail.com>
2026-03-24 07:16:43 +0530

    add: delay for error notification component

--- frontend/components/ErrorNotification.tsx
+++ frontend/components/ErrorNotification.tsx
@@ -1,7 +1,25 @@
-const ErrorNotification = ({ error }: { error: string }) => {
+'use client'
+import { useEffect, useState } from 'react'
+
+const ErrorNotification = ({
+  error,
+  delay,
+}: {
+  error: string
+  delay: number
+}) => {
+  const [showError, setShowError] = useState(error)
+  //use effect for error removing
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setShowError('')
+      }, delay)
+    }
+  }, [error])
   return (
     <p
-      className={`fixed top-0 ${error ? 'right-2' : '-right-100'} rounded-xl border bg-red-800 p-3 text-white transition-all duration-300`}
+      className={`fixed top-0 ${showError ? 'right-2' : '-right-100'} rounded-xl border bg-red-800 p-3 text-white transition-all duration-300`}
     >
       {error}
     </p>
--- frontend/components/register-page/CreateAccount.tsx
+++ frontend/components/register-page/CreateAccount.tsx
@@ -67,18 +67,9 @@ const CreateAccount = () => {
     }
   }
 
-  //use effect for error removing
-  useEffect(() => {
-    if (error) {
-      setTimeout(() => {
-        setError('')
-      }, 3000)
-    }
-  }, [error])
-
   return (
     <div className="px-10 py-8">
-      <ErrorNotification error={error} />
+      <ErrorNotification error={error} delay={3000} />
       <h1 className="text-xl font-bold text-[#C84D1C] md:hidden">QuickBites</h1>
       <div>
         <h2 className="text-2xl font-black">Create Account</h2>
