5962b22e9e30ded4076843885c660391628dd852
Nikhil Randhawa <ns9013604867@gmail.com>
2026-03-25 18:11:01 +0530

    fix:LCP warning from next.js update: backend dev port to 4000 frontend api calls as well

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://localhost:5173"],
+    origin: ["http://localhost:3000", "https://localhost:5173"],
     credentials: true,
   }),
 );
--- Backend/src/middlewares/logger.middleware.js
+++ Backend/src/middlewares/logger.middleware.js
@@ -1,5 +1,7 @@
 import pinoHttp from "pino-http";
 import "pino-pretty";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const httpLogger = pinoHttp({
   transport:
